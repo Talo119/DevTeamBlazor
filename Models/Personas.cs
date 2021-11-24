8427a99651a3fc1b4900c9371483646b27c45857
Carlos Motiño <carlos.motino@elcatex.com>
2021-11-24 08:16:45 -0600

    Initial commit

---
+++ Models/Personas.cs
@@ -0,0 +1,10 @@
+﻿namespace EjercicioDevTeam.Models
+{
+    public class Personas
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+    }
+}
